c72ddfd56578f514b71d2504992e7f26d11023ca
rambham <rambham@072a29c2-3dfb-cd76-6e5b-f77b2e1f7b25>
2011-10-03 02:13:34 +0000

    more doxygen progress - now autotool generates makefiles that call doxygen, though it does not yet work, and the doxyfile is created from doxyfile.in

--- trunk/autoclean.sh
+++ trunk/autoclean.sh
@@ -6,12 +6,17 @@
 
 rm -f aclocal.m4 configure depcomp
 rm -f install-sh ltmain.sh missing Makefile Makefile.in *~
-rm -fr autom4te.cache .deps
 rm -f config.log config.status libtool cpptoken-*.tar.gz
 rm -f m4/ltsugar.m4 m4/libtool.m4 m4/ltversion.m4 m4/lt~obsolete.m4
 rm -f m4/ltoptions.m4
 rm -f include/Makefile.in
-rm -fr src/.libs src/.deps
-rm -fr include/Makefile include/Makefile.in
+
 rm -f src/*.o src/*.lo src/Makefile.in src/*~ src/utests src/*.a src/*.la
 rm -f src/stamp-h1 src/config.h src/Makefile
+rm -f include/Makefile include/Makefile.in
+
+
+rm -fr autom4te.cache .deps src/.libs src/.deps docs html latex
+
+rm -f texput.log
+
